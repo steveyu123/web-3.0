3d54cb44279a53d015236107bd75bc726a42ab52
Steve <steveyu123@users.noreply.github.com>
2017-01-16 11:51:56 -0800

    Add files via upload

--- react/youtube_browser/search_bar.js
+++ react/youtube_browser/search_bar.js
@@ -62,7 +62,7 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => onInputChange(event.target.value)} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
   }
